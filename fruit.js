a52022a30f6164f8def4f5ed2e0664b81a705478
Phillip <phv9495@saintjoe.edu>
2015-02-16 12:12:59 -0500

    Added the code to implement a JSON array of objects.

---
+++ fruit.js
@@ -0,0 +1,10 @@
+//Phil Hofer
+//
+
+{"fruit":[
+  {"Name":"Apple", "Color":"Red"},
+  {"Name":"Banana", "Color":"Yellow"},
+  {"Name":"Orange", "Color":"Orange"},
+  {"Name":"Pineapple", "Color":"Brown"},
+  {"Name":"Lemon", "Color":"Yellow"}
+]}
